6d78d769e039179d6892fe679531b52e54979e29
akradadiya28 <akradadiya28@gmail.com>
2024-03-10 22:26:42 +0530

    hamburgar added

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#menubar").click(function () {
+        $("#hamburgar").slideToggle("fast");
+        // $(this).addClass("active");
+        // $(this).parent("li").siblings("li").children(".dropdown-menu").slideUp("fast");
+        // $(this).parent("li").siblings("li").children(".dropdown").removeClass("active")
+    })
+})
