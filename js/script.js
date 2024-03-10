97ece96c5e164b6c6f0e49daf55868bbcc7c6c5e
akradadiya28 <akradadiya28@gmail.com>
2024-03-10 23:08:11 +0530

    hamburgar activate

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     $("#menubar").click(function () {
         $("#hamburgar").slideToggle("fast");
-        // $(this).addClass("active");
-        // $(this).parent("li").siblings("li").children(".dropdown-menu").slideUp("fast");
-        // $(this).parent("li").siblings("li").children(".dropdown").removeClass("active")
+    })
+    $("#closeBtn").click(function () {
+        $("#hamburgar").slideToggle("fast");
     })
 })
